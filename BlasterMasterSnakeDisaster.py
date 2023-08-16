e0747100c84723f6d61ea1a9dccf88956b42799f
mateopascual <mateojspascual@gmail.com>
2023-08-16 11:54:19 -0700

    Game class Created

--- BlasterMasterSnakeDisaster.py
+++ BlasterMasterSnakeDisaster.py
@@ -1,8 +1,8 @@
 import pygame
 pygame.init()
-
+#set screen up
 display = pygame.display.set_mode((1280,720)) 
-
+#while run condition is true keep screen open
 run = True
 while run:
     display.fill("black")
