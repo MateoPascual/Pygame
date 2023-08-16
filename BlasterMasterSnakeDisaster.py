f987f1cfe001bc942bf3b07815ce1a281348b9f0
mateopascual <mateojspascual@gmail.com>
2023-08-16 12:56:34 -0700

    sprite update

--- BlasterMasterSnakeDisaster.py
+++ BlasterMasterSnakeDisaster.py
@@ -1,45 +1,42 @@
 import pygame
 
-#set screen up
-
-
-#game class
 class Game:
     def __init__(self) -> None:
         pygame.init()
         self.running = True
-        self.display = pygame.display.set_mode((1280,720)) 
-        self.sprites = self.load_sprite()
+        self.screen = pygame.display.set_mode((1280, 720))
+        self.sprites = self.load_sprites()
 
     def poll_events(self) -> None:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                self.run = False
+                self.running = False
 
     def update(self) -> None:
         pass
 
     def render(self) -> None:
-        self.display.fill("black")
+        self.screen.fill("black")
+        self.screen.blit(self.sprites["backround"],(0,0))
+        self.screen.blit(self.sprites["spaceship"],(100,100))
+
+
         pygame.display.update()
-        #render 
 
     def run(self) -> None:
         while self.running:
-            self.poll_events() 
+            self.poll_events()
             self.update()
             self.render()
         pygame.quit()
 
-    def load_sprite(self) -> dict:
+    def load_sprites(self) -> dict:
         sprites = {}
-
-        sprites["player"] = pygame.image.load("pygame/ship.png").convert_alpha()
-
+        sprites["spaceship"]=pygame.image.load("gfx/ship.png").convert_alpha()
+        sprites["backround"]=pygame.image.load("gfx/simple_game_bg.png").convert_alpha()
+        sprites["spaceship"]=pygame.transform.scale(sprites["spaceship"],(40,40))
         return sprites
-    
 
 
-
-g= Game()
-g.run
+g = Game()
+g.run()
